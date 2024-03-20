de8720782c4e749a20a8f41cc8fb7e04d93695b3
Navneet Anand <n4navneetanand@gmail.com>
2024-03-21 00:33:25 +0530

    init commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Charts from "@/components/Charts";
+import Projects from "@/components/Projects";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Projects />
+      <Charts />
+    </div>
+  );
+}
